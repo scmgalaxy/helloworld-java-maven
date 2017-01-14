d60fc628d297933727ff9c031c797dc1a4cdb127
scmgalaxy <rajesh@scmgalaxy.com>
2017-01-14 16:30:23 +0530

    HSBC-1 UI fix
    HSBC-1 UI fixing sdad sd

--- src/main/java/com/scmgalaxy/mavensample/App.java
+++ src/main/java/com/scmgalaxy/mavensample/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!12355" );
+        System.out.println( "Hello World!123557" );
     }
 }
