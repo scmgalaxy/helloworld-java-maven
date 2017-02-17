d90c4cd47cd7334484cdd95d6e557374e36b9fff
scmgalaxy <rajesh@scmgalaxy.com>
2017-02-17 15:48:30 +0530

    CTSX-2
    CTSX-2 Fix for google.com

--- src/main/java/com/scmgalaxy/mavensample/App.java
+++ src/main/java/com/scmgalaxy/mavensample/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!1235575646546" );
+        System.out.println( "Hello World!1235575646546ddd" );
     }
 }
