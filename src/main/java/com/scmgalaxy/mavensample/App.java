dba9deda9e3a3875ef39a7a5b8504171f689df63
scmgalaxy <rajesh@scmgalaxy.com>
2017-03-31 10:41:44 +0530

    MAH-2 fixed
    MAH-2 fixed

--- src/main/java/com/scmgalaxy/mavensample/App.java
+++ src/main/java/com/scmgalaxy/mavensample/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!1235575646546ddd" );
+        System.out.println( "Hello World! MAH-2--d" );
     }
 }
