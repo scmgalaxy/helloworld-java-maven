2607e5de05e6f4257c1b3125fabf0aa83886a0fa
metlogulzar <90175689+metlogulzar@users.noreply.github.com>
2021-10-10 15:24:50 +0500

    mah-1 fixed
    mah-1 fixed

--- src/main/java/com/scmgalaxy/mavensample/App.java
+++ src/main/java/com/scmgalaxy/mavensample/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World RefX! MAH-2--d" );
+        System.out.println( "Hello World RefX! mah-1..d" );
     }
 }
